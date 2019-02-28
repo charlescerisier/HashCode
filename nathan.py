ba02fa7b2b0e9ee57a2b39b0220ff3336a728b7a
Nathan Le Granvallet <nathan@legranvallet.fr>
2019-02-28 19:02:03 +0100

    + parse

--- nathan.py
+++ nathan.py
@@ -1,1 +1,34 @@
-print("Hello world")
+fileName = "a_example.txt"
+
+nbPics = 0
+pics = []
+i = 0
+
+# PICS LINE FORMAT
+# PICS : 
+# [
+#    {
+#       "nbTags": 1
+#       "tags": ["tag1", "tag2", "tag3"]
+#       "type": 'H'
+#     }
+# ]
+
+with open(fileName) as file:
+    print("File " + fileName + " opened")
+
+    for line in file:
+        print(line)
+        if (i == 0):
+            nbPics = line
+        else:
+            picLine = line.replace('\n', '').split(' ')
+            pic = {}
+            pic['type'] = picLine[0]
+            pic['nbTags'] = picLine[1]
+            pic['tags'] = picLine[2:]
+            pics.append(pic)
+        
+        i += 1
+
+    print(pics)
